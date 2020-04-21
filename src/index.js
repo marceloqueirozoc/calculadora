cdd1363ba6c4162650d6cc881822f1e070b7b341
Marcelo Queiroz <marceloqueirozoc@gmail.com>
2020-04-21 16:18:57 -0300

    Calculadora Finalizada

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
+import Calculator from './main/Calculator'
+
+ReactDom.render(
+    <div>
+        <h1>Calculadora</h1>
+        <Calculator />
+    </div>
+    
+    
+, document.getElementById('root'))
